74d7a787ba1f244dc38ee6ce1d912c863832c133
Aadish <addyj2908@gmail.com>
2020-10-11 17:28:04 +0530

    Made Sign In Function

--- Blueprints/form.js
+++ Blueprints/form.js
@@ -3,8 +3,7 @@ class Form {
         this.signUp = createButton('Sign Up');
         this.signUp.position(100, 100);
         this.signUp.mousePressed(()=>{
-            this.signUp.hide();
-            this.signIn.hide();
+            this.hideAll()
             this.back.show();
             this.checkUsername.show();
             this.enterUsername.show();
@@ -12,8 +11,7 @@ class Form {
         this.signIn = createButton('Sign In');
         this.signIn.position(200, 100);
         this.signIn.mousePressed(()=>{
-            this.signUp.hide();
-            this.signIn.hide();
+            this.hideAll();
             this.back.show();
             this.enterPassword.show();
             this.enterUsername.show();
@@ -22,20 +20,19 @@ class Form {
         this.back = createButton('Back');
         this.back.position(100, 100);
         this.back.mousePressed(()=>{this.signUpPage();});
-        this.back.hide();
+        this.backHome = createButton('Back');
+        this.backHome.position(100, 100);
+        this.backHome.mousePressed(()=>{this.homePage();});
         this.enterUsername = createInput('Username');
         this.enterUsername.position(100, 200);
-        this.enterUsername.hide();
         this.enterPassword = createInput('Password');
         this.enterPassword.position(300, 200);
-        this.enterPassword.hide();
         this.incorrect = createElement('h5');
         this.incorrect.position(100, 400);
         this.incorrect.hide();
         this.incorrect.html('Username or password incorrect');
         this.enter = createButton('Enter');
         this.enter.position(100, 300);
-        this.enter.hide();
         this.enter.mousePressed(()=>{
             database.ref('Players').once('value').then((val)=>{
                 if (!checkUsername(val.val(), this.enterUsername.value())) {
@@ -52,7 +49,6 @@ class Form {
             });
         })
         this.create = createButton('Create Account');
-        this.create.hide();
         this.create.position(100, 300);
         this.create.mousePressed(()=>{
             this.homePage();
@@ -65,19 +61,16 @@ class Form {
         })
         this.usernameTaken = createElement('h5');
         this.usernameTaken.position(100, 400);
-        this.usernameTaken.hide();
         this.usernameTaken.html('This Username is already taken');
         this.checkUsername = createButton('Next');
-        this.checkUsername.hide();
         this.checkUsername.position(100, 300);
         this.checkUsername.mousePressed(()=>{
             database.ref('Players').once('value').then((val)=>{
                 if (checkUsername(val.val(), this.enterUsername.value())) {
-                    this.checkUsername.hide();
-                    this.enterUsername.hide();
+                    this.hideAll()
                     this.enterPassword.show();
-                    this.create.show();
-                    this.usernameTaken.hide();
+                    this.create.show();;
+                    this.back.show();
                 } else {
                     this.usernameTaken.show();
                 }
@@ -86,59 +79,103 @@ class Form {
         this.signOut = createButton('Sign Out');
         this.signOut.position(100, 100);
         this.signOut.mousePressed(()=>{this.signUpPage();});
-        this.signOut.hide();
         this.delete = createButton('Delete Account');
-        this.delete.hide();
-        this.delete.position(100, 500);
+        this.delete.position(100, 700);
         this.delete.mousePressed(()=>{
+            this.hideAll()
             this.deleteSure.show()
             this.deleteYes.show();
             this.deleteNo.show();
-            this.delete.hide();
-            this.signOut.hide();
-            name = null;
         });
         this.deleteYes = createButton('Yes');
-        this.deleteYes.hide();
         this.deleteYes.position(100, 300);
         this.deleteYes.mousePressed(()=>{
+            database.ref('Players/' + name).remove();
             this.signUpPage();
-            database.ref('Players/' + this.enterUsername.value()).remove();
         });
         this.deleteNo = createButton('No');
-        this.deleteNo.hide();
         this.deleteNo.position(100, 200);
         this.deleteNo.mousePressed(()=>{this.homePage();});
         this.deleteSure = createElement('h3');
-        this.deleteSure.hide();
         this.deleteSure.position(100, 100);
         this.deleteSure.html('ARE YOU SURE YOU WANT TO DELETE YOUR ACCOUNT. IT WILL BE GONE FOREVER');
+        this.changePassword = createButton('Change Password');
+        this.changePassword.position(100, 500);
+        this.changePassword.mousePressed(()=>{
+            this.hideAll();
+            this.enterPassword.show();
+            this.confirmPassword.show();
+            this.backHome.show();
+        });
+        this.confirmPassword = createButton('Confirm');
+        this.confirmPassword.position(300, 300);
+        this.confirmPassword.mousePressed(()=>{
+            database.ref('Players/' + name).update({'Password' : this.enterPassword.value()});
+            this.homePage();
+        });
+        this.changeUsername = createButton('Change Username');
+        this.changeUsername.position(100, 600);
+        this.changeUsername.mousePressed(()=>{
+            this.hideAll();
+            this.enterUsername.show();
+            this.confirmUsername.show();
+            this.backHome.show();
+        });
+        this.confirmUsername = createButton('Confirm');
+        this.confirmUsername.position(100, 300);
+        this.confirmUsername.mousePressed(()=>{
+            database.ref('Players').once('value').then((val)=>{
+                if (checkUsername(val.val(), this.enterUsername.value())) {
+                    database.ref('Players/' + name).once('value').then((val)=>{
+                        database.ref('Players/' + this.enterUsername.value()).update({
+                            'Password' : val.val().Password,
+                            'Score' : val.val().Score
+                        });
+                    });
+                    oldName = name;
+                    this.homePage();
+                    wait[0] = true;
+                } else {
+                    this.usernameTaken.show();
+                }
+            });
+        });
+        this.leaderBoards = createButton('Leader Boards');
+        this.leaderBoards.position(100, 400);
+        this.leaderBoards.mousePressed(()=>{this.leaderBoardsF('Total')});
+        this.signUpPage();
     }
     signUpPage() {
+        this.hideAll()
         this.signUp.show();
         this.signIn.show();
-        this.back.hide();
-        this.enterPassword.hide();
-        this.enterUsername.hide();
-        this.incorrect.hide();
-        this.enter.hide()
-        this.create.hide();
-        this.checkUsername.hide();
-        this.usernameTaken.hide();
-        this.signOut.hide();
-        this.delete.hide()
-        this.deleteSure.hide()
-        this.deleteYes.hide();
-        this.deleteNo.hide();
         if (name === null || name === undefined) {
             name = this.enterUsername.value();
         }
         database.ref('Players/' + name + '/Score').off();
         name = null;
+        state = "Sign Up";
     }
     homePage() {
+        this.hideAll()
         this.signOut.show();
-        this.delete.show()
+        this.delete.show();
+        this.changePassword.show();
+        this.changeUsername.show();
+        database.ref('Players/' + name + '/Score').off();
+        name = this.enterUsername.value();
+        database.ref('Players/' + name + '/Score').once('value').then(function (data) {
+            score = data.val();
+        });
+        database.ref('Players/' + name + '/Score').on('value', function (data) {
+            score = data.val();
+        });
+        state = "Home";
+    }
+    hideAll() {
+        state = null;
+        this.signOut.hide();
+        this.delete.hide()
         this.signUp.hide();
         this.signIn.hide();
         this.back.hide();
@@ -152,16 +189,28 @@ class Form {
         this.deleteSure.hide()
         this.deleteYes.hide();
         this.deleteNo.hide();
-        if (name === null || name === undefined) {
-            name = this.enterUsername.value();
+        this.changePassword.hide();
+        this.changeUsername.hide();
+        this.confirmPassword.hide();
+        this.confirmUsername.hide();
+        this.checkUsername.hide();
+        this.backHome.hide();
+        this.leaderBoards.hide();
+    }
+    leaderBoardsF(type) {
+        let LS = [];
+        database.ref('Players').once('value').then((val)=>{
+            for (let loop1 in val.val()) {
+                LS.push({
+                    'name' : loop1,
+                    'score' : loop1.Score[type]
+                });
+            }
+        });
+        let tops = [];
+        for (let loop1 in LS) {
+            
         }
-        database.ref('Players/' + name + '/Score').off();
-        database.ref('Players/' + name + '/Score').on('value', function (data) {
-            score = data.val();
-        });
-        database.ref('Players/' + name + '/Score').once('value').then(function (data) {
-            score = data.val();
-        });
     }
 }
 
--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let form, database, name, score;
+let form, database, name, score, state, wait = [], oldName, topper = [];
 
 function setup () {
     createCanvas(400, 400)
@@ -8,7 +8,17 @@ function setup () {
 
 function draw() {
     background(255, 255, 255);
-    if (name !== undefined && name !== null) {
+    if (state === "Home" && score !== undefined && score !== null) {
         text("Score = " + score.Total, 100, 200);
     }
+    if (wait[0]) {
+        database.ref('Players/' + name).once('value').then((val)=>{
+            if (val.val() !== undefined && val.val() !== null) {
+                wait[0] = false;
+                database.ref('Players/' + oldName).remove();
+                form.homePage();
+                oldName = null;
+            }
+        });
+    } 
 }
